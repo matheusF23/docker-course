b52c2b4f1c87bfa3dc38e4c340d244243d1748d2
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2021-01-24 13:03:17 -0300

    Atualization

--- node-mongo-compose/backend/app.js
+++ node-mongo-compose/backend/app.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const restful = require('node-restful');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const server = express();
 const mongoose = restful.mongoose
 
 // Database
-mongoose.Promise = global.Promise
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://db/mydb');
 
-// test
-server.get('/', (req, res, next) => res.send('Backend'));
+// Middleware
+server.use(bodyParser.urlencoded({ extended:true }));
+server.use(bodyParser.json());
+server.use(cors());
+
+// ODM
+const Client = restful.model('Client', {
+  name: { type: String, required: true }
+});
+
+// Rest API
+Client.methods(['get', 'post', 'put', 'delete']);
+Client.updateOptions({ new: true, runValidators: true });
+
+// Routes
+Client.register(server, '/clients');
 
 // Start server
 server.listen(3000);
